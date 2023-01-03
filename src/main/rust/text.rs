d8b31ad9d6a4960cb3878de8a23ac80c38d90c1e
Hannes Moser <box@hannesmoser.at>
2023-01-03 11:20:17 +0100

    feat: create doc, transaction and text, append string and print

---
+++ src/main/rust/text.rs
@@ -0,0 +1,30 @@
+use std::cell::RefCell;
+use yrs::{GetString, TextRef, Text as YrsText};
+use crate::transaction::Transaction;
+
+pub(crate) struct Text(RefCell<TextRef>);
+
+impl Text {
+  pub(crate) fn append(&self, tx: &Transaction, text: String) {
+    let mut tx = tx.transaction();
+    let tx = tx.as_mut().unwrap();
+
+    self.0.borrow_mut().push(tx, text.as_str());
+  }
+
+  pub(crate) fn get_string(&self, tx: &Transaction) -> String {
+    let mut tx = tx.transaction();
+    let tx = tx.as_mut().unwrap();
+
+    self.0.borrow().get_string(tx)
+  }
+}
+
+unsafe impl Send for Text {}
+unsafe impl Sync for Text {}
+
+impl From<TextRef> for Text {
+  fn from(value: TextRef) -> Self {
+    Text(RefCell::from(value))
+  }
+}
